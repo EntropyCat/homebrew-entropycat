c0e1095c947b404a288766f8f0629e4467d943fe
Alex-Momotov-BA <momotovalex@gmail.com>
2026-06-23 22:26:59 +0100

    entropycat 0.2.7

--- Formula/entropycat.rb
+++ Formula/entropycat.rb
@@ -1,17 +1,17 @@
 class Entropycat < Formula
   desc "Real-time data quality agent for Kafka streams"
   homepage "https://entropycat.io"
-  version "0.2.6"
+  version "0.2.7"
   license "Proprietary"
 
   on_macos do
     on_arm do
       url "https://github.com/EntropyCat/entropycat/releases/download/v#{version}/entropycat_#{version}_darwin_arm64.zip"
-      sha256 "d1efb27ed943de52b82a82e112724e71cb75aa3a65f65ca7a92a39e12fd3397f"
+      sha256 "53f5d1747564723f369d5574b2550de15d246d03490e56cdec9758c77836bfa7"
     end
     on_intel do
       url "https://github.com/EntropyCat/entropycat/releases/download/v#{version}/entropycat_#{version}_darwin_x86_64.zip"
-      sha256 "ec380588397960cd690239728db215d8da221e408b88e57f5647ffe5b25c7f9a"
+      sha256 "b06f7fcbde4dd6bd48885e3338fdafbcee1fb6ba25063cd5bc5b046517568706"
     end
   end
 
